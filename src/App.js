997e491a260f0836c9417a90e99008bf51a158e1
hyunyeee <5004ohy@naver.com>
2024-04-03 02:40:14 +0900

    setting: react-router-dom 세팅

--- src/App.js
+++ src/App.js
@@ -1,3 +1,14 @@
-function App() {}
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from './pages/Main';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
