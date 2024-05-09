45214de6a3ec5ee09392f82f76ac28e0307b463a
hyunyeee <5004ohy@naver.com>
2024-05-09 15:05:41 +0900

    feat: 코드 실행 기능 구현 - #2
    - JavaScript 코드 실행 결과를 렌더링

--- src/components/codeEditer/CodeEditer.js
+++ src/components/codeEditer/CodeEditer.js
@@ -5,18 +5,44 @@ import runIcon from '../../assets/run.svg';
 
 function CodeEditer() {
   const editorRef = useRef(null);
-  const [result, setResult] = useState('// 코드를 입력해주세요');
+  const [result, setResult] = useState('');
 
   function handleEditorDidMount(editor) {
     editorRef.current = editor;
   }
-  function showValue() {
-    setResult(editorRef.current?.getValue());
+
+  function runCode() {
+    const code = editorRef.current?.getValue();
+    const consoleMessages = [];
+
+    // 기존 console.log 메서드 백업
+    const originalConsoleLog = console.log;
+
+    // 새로운 console.log 정의, 메시지를 배열에 추가
+    console.log = function (...args) {
+      consoleMessages.push(args.join(' '));
+      originalConsoleLog.apply(console, args);
+    };
+
+    try {
+      // eslint-disable-next-line no-eval
+      eval(code);
+    } catch (error) {
+      consoleMessages.push(`Error: ${error.message}`);
+    }
+
+    // `console.log`를 원래 기능으로 복원
+    console.log = originalConsoleLog;
+
+    // 수집된 메시지들을 하나의 문자열로 합쳐서 state에 저장
+    setResult(consoleMessages.join('\n'));
+    console.log(result);
   }
+
   return (
     <>
       <Editor
-        value={result}
+        value="// 코드를 입력해주세요"
         height="50vh"
         language="javascript"
         theme="vs-dark"
@@ -30,7 +56,7 @@ function CodeEditer() {
         onMount={handleEditorDidMount}
       />
       <ResultContainer>
-        <RunButton type="button" onClick={showValue}>
+        <RunButton type="button" onClick={runCode}>
           <img src={runIcon} alt="실행 아이콘" />
           Run
         </RunButton>
@@ -59,5 +85,8 @@ const RunButton = styled.button`
 `;
 const Result = styled.div`
   min-height: 300px;
+  padding: 20px;
+  ${({ theme }) => theme.typographies.DEFAULT_TXT}
+  white-space: pre;
   background-color: white;
 `;
