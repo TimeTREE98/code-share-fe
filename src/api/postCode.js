8d0aeb42faa9ce7784ad4667551d8ac19722123d
hyunyeee <5004ohy@naver.com>
2024-05-20 14:23:13 +0900

    fix: postCode API withCredentials: false로 설정 - #4

--- src/api/postCode.js
+++ src/api/postCode.js
@@ -2,10 +2,14 @@ import { Axios } from './Axios';
 
 export const postCode = async (code) => {
   try {
-    const response = await Axios.post(`/judge0/submissions?base64_encoded=true&wait=true`, {
-      source_code: code,
-      language_id: 63,
-    });
+    const response = await Axios.post(
+      `/judge0/submissions?base64_encoded=true&wait=true`,
+      {
+        source_code: code,
+        language_id: 63,
+      },
+      { withCredentials: false },
+    );
     return response.data;
   } catch (error) {
     return error;
