e3427a17cc173a2e127136054617069ab7b219f1
zzzzzuuuuu <agu0830@naver.com>
2024-05-24 01:09:34 +0900

    feat: 룸 생성 모달 구현 - #15

---
+++ src/components/room/CreateRoomModal.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+const CreateRoomModal = (props) => {
+  const { setModal } = props;
+  const [title, setTitle] = useState('');
+  const closeModal = () => {
+    setModal(false);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault(); // 폼 제출시에 새로고침 방지함
+    closeModal();
+  };
+
+  useEffect(() => {
+    console.log(title);
+  }, [title]);
+
+  return (
+    <ModalContainer>
+      <ModalText>방 제목을 입력해주세요.</ModalText>
+      <form onSubmit={handleSubmit}>
+        <InputContainer>
+          <TitleInput type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+          <TitleBtn type="submit">+</TitleBtn>
+        </InputContainer>
+      </form>
+    </ModalContainer>
+  );
+};
+
+export default CreateRoomModal;
+
+const ModalContainer = styled.div`
+  position: fixed;
+  top: 25%;
+  bottom: 0;
+  width: 500px;
+  height: 400px;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid black;
+`;
+
+const ModalText = styled.p`
+  font-size: 24px;
+`;
+
+const InputContainer = styled.div`
+  display: flex;
+  margin-top: 20px;
+  gap: 10px;
+`;
+
+const TitleInput = styled.input`
+  width: 250px;
+  height: 40px;
+  border: 1px solid lightgray;
+`;
+
+const TitleBtn = styled.button`
+  width: 40px;
+  height: 40px;
+  background: forestgreen;
+  font-size: 20px;
+  color: white;
+`;
